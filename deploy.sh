e80a0d6ac574f17042f360ac87a730b2ca17ca83
Nick Lothian <nick@verida.io>
2022-07-14 08:41:53 +0930

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
-npm run build
-aws s3 sync dist s3://demo.testnet.verida.io --profile verida-testnet --delete
-aws cloudfront create-invalidation --distribution-id EWEA2O1K4RSLI --profile verida-testnet --paths "/*"
+# Do this EXACTLY. 
+# If you do "smart" things like adding messages to the merge, main branch ends up ahead of develop
+# This is because a comment forces a merge commmit, and we just want to fast-foward. 
+# See "MERGING TAG" under https://git-scm.com/docs/git-merge
+git checkout develop
+git pull
+git checkout main
+git pull
+git merge develop --ff-only 
+git push
