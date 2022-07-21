3a9aab634ba648e2c9d43ea735c1b0c29dd6098a
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2022-07-21 22:49:17 +0100

    fix: added back the openUrl prop #59

--- src/components/navigation/UserMenu.vue
+++ src/components/navigation/UserMenu.vue
@@ -5,6 +5,7 @@
     </a>
     <vda-account
       :logo="logo"
+      :openUrl="openUrl"
       :contextName="contextName"
       @onError="onError"
       @onLogout="disconnect"
@@ -31,6 +32,7 @@ export default defineComponent({
     return {
       contextName: envKeys.VUE_APP_CONTEXT_NAME,
       logo: envKeys.VUE_APP_LOGO_URL,
+      openUrl: window.origin,
     };
   },
   computed: {
